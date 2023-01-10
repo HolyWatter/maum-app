989e03abbd238b4699dbb67a6638e81bd7d9f8a6
HolyWatter <qktjdtn97@icloud.com>
2023-01-10 14:16:47 +0900

    초기세팅

---
+++ README.md
@@ -0,0 +1,5 @@
+## 마음 앱
+
+### nextron + react + typescript + firebase
+### styled with tailwind css
+
---
+++ renderer/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>메인페이지</>
+  );
+}
+
+export default Home;
