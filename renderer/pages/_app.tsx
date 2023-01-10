0d02f67c83013096cbee7dace7a92f3777a4d218
HolyWatter <qktjdtn97@icloud.com>
2023-01-10 15:15:00 +0900

    프로젝트 세팅 (firebase, recoil 추가설치)

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-
+import firebase from './api/firebase/firebase';
 import '../styles/globals.css';
+
+console.log(firebase)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
